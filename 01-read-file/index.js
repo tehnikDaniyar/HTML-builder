e9c943783a1bcbb29dd244f18ccf7477f00a283d
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2025-01-21 15:17:28 +0600

    fix: replace readFile on readStream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,7 +3,17 @@ const path = require('path');
 
 const absolutePath = path.join(__dirname, 'text.txt');
 
-fs.readFile(absolutePath, (err, data) => {
-  if (err) throw err;
-  console.log(data.toString());
+const readStream = fs.createReadStream(absolutePath, { encoding: "utf-8" });
+
+readStream.on('data', (chunk) => {
+   console.log(chunk);
 });
+
+
+
+// fs.readFile(absolutePath, (err, data) => {
+//    if (err) throw err;
+//    console.log(data.toString());
+// });
+
+
