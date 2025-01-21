3ee171326d6aceaee3f957e26234f3678f8c0050
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2025-01-21 18:50:35 +0600

    fix05-merge-styles: fix bundleCss function

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,18 +1,27 @@
 const fs = require('fs').promises;
 const path = require('path');
+const sourceCss = path.join(__dirname, 'styles');
+const targetCSs = path.join(__dirname, 'project-dist/bundle.css');
 
-bundleCss();
+
+bundleCss(sourceCss, targetCSs);
+
+async function bundleCss(source, target) {
+   let res = '';
+   const files = await fs.readdir(source)
+
+   for (const file of files) {
+      const pathFile = path.join(source, file);
+      const extName = path.extname(pathFile);
+
+      if (extName === '.css') {
+         const data = await fs.readFile(pathFile);
+         res += data;
+      };
+   };
+
+   await fs.writeFile(target, res);
+};
 
 
-async function bundleCss() {
-   let data1 = '';
-   const path1 = path.join(__dirname, 'styles/style-1.css');
-   const path2 = path.join(__dirname, 'styles/style-2.css');
-   const path3 = path.join(__dirname, 'styles/style-3.css');
 
-   data1 += await fs.readFile(path1, 'utf8');
-   data1 += await fs.readFile(path2, 'utf8');
-   data1 += await fs.readFile(path3, 'utf8');
-
-   await fs.writeFile(path.join(__dirname, 'project-dist/bundle.css'), data1);
-}
