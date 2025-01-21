6e1c87dad3ac40107addc83ddd30ad494630ab84
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2025-01-21 15:27:50 +0600

    fix: added checking input 'exit'

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,12 +6,18 @@ const absolutePath = path.join(__dirname, 'text.txt');
 console.log('HELLO DEVOLOPMER!!!!');
 
 stdin.on('data', (data) => {
-  fs.appendFile(absolutePath, data.toString(), (err) => {
-    if (err) throw err;
-  });
+
+   if (data.toString().trim() === 'exit') {
+      console.log('BY BY go slip my litle devoloper');
+      process.exit();
+   } else {
+      fs.appendFile(absolutePath, data.toString(), (err) => {
+         if (err) throw err;
+      });
+   };
 });
 
 process.on('SIGINT', () => {
-  console.log('BY BY go slip my litle devoloper');
-  process.exit();
+   console.log('BY BY go slip my litle devoloper');
+   process.exit();
 });
