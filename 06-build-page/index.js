4ed5cac6d42db6afe7b7a246c7a0c66ca6c526d3
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2025-01-21 19:03:43 +0600

    fix: fixed bundleHTML function

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,91 +1,100 @@
 const fs = require('fs').promises;
 const path = require('path');
-const dirPath = path.join(__dirname, '/project-dist');
+const targetFolder = path.join(__dirname, '/project-dist');
 const sourseFiles = path.join(__dirname, 'assets');
+const sourceCss = path.join(__dirname, 'styles');
+const targetCss = path.join(targetFolder, 'style.css');
 
 bundleProject();
 
 async function createDir(dirPath) {
-  try {
-    await fs.access(dirPath);
-  } catch (error) {
-    fs.mkdir(dirPath);
-  }
+   try {
+      await fs.access(dirPath);
+      await fs.rm(dirPath, { recursive: true });
+      fs.mkdir(dirPath);
+   } catch (error) {
+      fs.mkdir(dirPath);
+   }
 }
 
-async function bundleCss() {
-  let data1 = '';
-  const path1 = path.join(__dirname, 'styles/01-header.css');
-  const path2 = path.join(__dirname, 'styles/02-main.css');
-  const path3 = path.join(__dirname, 'styles/03-footer.css');
+async function bundleCss(source, target) {
+   let res = '';
+   const files = await fs.readdir(source)
 
-  data1 += await fs.readFile(path1, 'utf8');
-  data1 += await fs.readFile(path2, 'utf8');
-  data1 += await fs.readFile(path3, 'utf8');
+   for (const file of files) {
+      const pathFile = path.join(source, file);
+      const extName = path.extname(pathFile);
 
-  await fs.writeFile(path.join(__dirname, 'project-dist/style.css'), data1);
-}
+      if (extName === '.css') {
+         const data = await fs.readFile(pathFile);
+         res += data;
+      };
+   };
+
+   await fs.writeFile(target, res);
+};
 
 async function bundleHtml() {
-  let templateData = '';
-  let templateHeader = '';
-  let templateArticles = '';
-  let templateFooter = '';
+   let templateData = '';
+   let templateHeader = '';
+   let templateArticles = '';
+   let templateFooter = '';
 
-  templateData += await fs.readFile(
-    path.join(__dirname, 'template.html'),
-    'utf-8',
-  );
-  templateHeader += await fs.readFile(
-    path.join(__dirname, 'components/header.html'),
-    'utf-8',
-  );
-  templateArticles += await fs.readFile(
-    path.join(__dirname, 'components/articles.html'),
-    'utf-8',
-  );
-  templateFooter += await fs.readFile(
-    path.join(__dirname, 'components/footer.html'),
-    'utf-8',
-  );
+   templateData += await fs.readFile(
+      path.join(__dirname, 'template.html'),
+      'utf-8',
+   );
+   templateHeader += await fs.readFile(
+      path.join(__dirname, 'components/header.html'),
+      'utf-8',
+   );
+   templateArticles += await fs.readFile(
+      path.join(__dirname, 'components/articles.html'),
+      'utf-8',
+   );
+   templateFooter += await fs.readFile(
+      path.join(__dirname, 'components/footer.html'),
+      'utf-8',
+   );
 
-  replaceTemplate();
+   replaceTemplate();
 
-  await fs.writeFile(
-    path.join(__dirname, 'project-dist/index.html'),
-    templateData,
-  );
+   await fs.writeFile(
+      path.join(__dirname, 'project-dist/index.html'),
+      templateData,
+   );
 
-  function replaceTemplate() {
-    templateData = templateData.replace(/\{\{header\}\}/g, templateHeader);
-    templateData = templateData.replace(/\{\{footer\}\}/g, templateFooter);
-    templateData = templateData.replace(/\{\{articles\}\}/g, templateArticles);
-  }
+   function replaceTemplate() {
+      templateData = templateData.replace(/\{\{header\}\}/g, templateHeader);
+      templateData = templateData.replace(/\{\{footer\}\}/g, templateFooter);
+      templateData = templateData.replace(/\{\{articles\}\}/g, templateArticles);
+   }
 }
+
+
+async function copyDir(source, target) {
+   await createDir(target);
+
+   const files = await fs.readdir(source);
+
+   for (const file of files) {
+      const sourcePath = path.join(source, file);
+      const targetPath = path.join(target, file);
+
+      const stats = await fs.stat(sourcePath);
+
+      if (stats.isDirectory()) {
+         await copyDir(sourcePath, targetPath);
+      } else {
+         await fs.copyFile(sourcePath, targetPath);
+      }
+   }
+};
+
 
 async function bundleProject() {
-  await createDir(dirPath);
-  await bundleCss();
-  await bundleHtml();
-  await copyDirectory(sourseFiles, path.join(__dirname, 'project-dist/assets'));
-}
-
-async function copyDirectory(source, target) {
-  await createDir(target);
-
-  const files = await fs.readdir(source);
-
-  for (const file of files) {
-    const sourcePath = path.join(source, file);
-    const targetPath = path.join(target, file);
-
-    const stats = await fs.stat(sourcePath);
-
-    if (stats.isDirectory()) {
-      console.log(sourcePath, targetPath);
-      await copyDirectory(sourcePath, targetPath);
-    } else {
-      await fs.copyFile(sourcePath, targetPath);
-    }
-  }
+   await createDir(targetFolder);
+   await bundleCss(sourceCss, targetCss);
+   await bundleHtml();
+   await copyDir(sourseFiles, path.join(__dirname, 'project-dist/assets'));
 }
