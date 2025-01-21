62133522969365b0ee5eb3c244d03b8aaa3ada46
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2025-01-21 14:16:55 +0600

    feat: complete 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,39 @@
+const fs = require("fs").promises;
+const path = require("path");
+const source = path.join(__dirname, "secret-folder");
+
+
+readFiles(source);
+
+async function readFiles(source) {
+   const files = await fs.readdir(source);
+
+   for (const file of files) {
+      const pathFile = path.join(source, file);
+      const stat = await fs.stat(pathFile);
+
+      if (stat.isDirectory()) {
+         readFiles(pathFile)
+      };
+
+      if (stat.isFile()) {
+         console.log({
+            fileName: file,
+            fileSize: stat.size,
+            lastEdit: stat.birthtime
+         });
+      }
+
+
+   }
+
+}
+
+
+
+
+
+async function getInfoOfFile(path) {
+
+};
+
